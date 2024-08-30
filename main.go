dcffde95aa76a7a7b74df2d195d3e18a59bcb2d4
Rashaad0554 <112050207+Rashaad0554@users.noreply.github.com>
2024-08-30 19:23:02 -0400

    Added ability to insert "messages".
    Inserts messages replicating those that we would receive from EMQX for our project.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"log"
@@ -14,9 +13,14 @@ var server = "localhost"       // Your SQL Server hostname
 var mssql_port = 1433          // Default SQL Server port
 var user = "UofGIoT"           // Your SQL Server username
 var passwd = "Iq1sAd7AVVK5UUR" // Your SQL Server password
-var database = "emqx_data"     // Your database name
+var database = "emqx_data_test"     // Your database name
 
 var db *sql.DB
+
+type Message struct {
+	topic_sensor_name string
+	measurement       string
+}
 
 func main() {
 	// Form the connection string
@@ -38,6 +42,21 @@ func main() {
 
 	fmt.Println("Connected to SQL Server successfully!")
 	selectVersion()
+
+	fmt.Printf("\n\n\n")
+
+	msg1 := Message{"topic1", "1000"}
+	if tableInsert(db, msg1) == 0 {
+		fmt.Printf("Uh oh 1!")
+	} else {
+		fmt.Printf("msg1 added successfully!\n")
+	}
+	msg2 := Message{"topic2", "2000"}
+	if tableInsert(db, msg2) == 0 {
+		fmt.Printf("Uh oh 2!")
+	} else {
+		fmt.Printf("msg2 added successfully!\n")
+	}
 }
 
 func selectVersion() {
@@ -54,4 +73,43 @@ func selectVersion() {
 		log.Fatal("Scan failed: ", err)
 	}
 	fmt.Printf("%s\n", result)
+}
+
+func tableInsert(db *sql.DB, message Message) int {
+	topicIDQuery := `SELECT topicID FROM Topics WHERE topicName = @topicName`
+	row := db.QueryRow(topicIDQuery, sql.Named("topicName", message.topic_sensor_name))
+
+	var logID int
+	logInsertQuery := `INSERT INTO Logs (topicID, measurement) VALUES (@topicID, @measurement); SELECT SCOPE_IDENTITY();`
+
+	var topicID int
+	err := row.Scan(&topicID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// Handle the case where no rows are returned
+			topicInsertQuery := `INSERT INTO Topics (topicName) VALUES (@topicName); SELECT SCOPE_IDENTITY();`
+			err := db.QueryRow(topicInsertQuery, sql.Named("topicName", message.topic_sensor_name)).Scan(&topicID)
+			if err != nil {
+				log.Fatal("1: ", err)
+			}
+
+			err = db.QueryRow(logInsertQuery, sql.Named("topicID", topicID), sql.Named("measurement", message.measurement)).Scan(&logID)
+			if err != nil {
+				log.Fatal("2: ", err)
+			}
+
+			return int(logID)
+		} else {
+			log.Fatal("3: ", err) // Handle other possible errors
+			return 0
+		}
+	} else {
+		// Continue processing with the retrieved topicID
+		err := db.QueryRow(logInsertQuery, sql.Named("topicID", topicID), sql.Named("measurement", message.measurement)).Scan(&logID)
+		if err != nil {
+			log.Fatal("4: ", err)
+		}
+
+		return int(logID)
+	}
 }
